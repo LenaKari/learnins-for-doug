a1fc9c96cce75c74c8b805bbcf6fb0e2fa654f58
pair <pair@Lovelace>
2015-09-30 19:16:22 +0100

    Created example javascript

---
+++ pairing/assets/js/main.js
@@ -0,0 +1,12 @@
+// Create a sample variable for concat example
+var person = "Doug";
+
+// Create a function that displays alert
+var createAlert = function() {
+  alert('This message is for ' + person);
+};
+
+// Create a function that prints to console
+var printToConsole = function() {
+  console.log("This message is a secret and " + person + " should't look at it!");
+};
